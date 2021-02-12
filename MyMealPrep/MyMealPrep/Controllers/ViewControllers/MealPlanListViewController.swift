42a3e0998e5f87a39eeb8f6a67f8cc5f6f18222c
Jakob Haslam <jakehaslam38@gmail.com>
2021-02-12 16:30:25 -0700

    add MealPlan GUI updates + MealPlanList ViewController

---
+++ MyMealPrep/MyMealPrep/Controllers/ViewControllers/MealPlanListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MealPlanListViewController.swift
+//  MyMealPrep
+//
+//  Created by Jake Haslam on 2/12/21.
+//
+
+import UIKit
+
+class MealPlanListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
